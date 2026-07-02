5e1041ae7bbc555e244fdac78a689ab73b086dac
axel delor <axeldelor@gmail.com>
2026-07-02 12:52:19 +0200

    unit tests with Mockito added

--- src/test/java/com/example/testspringboot/service/AgencyServiceTest.java
+++ src/test/java/com/example/testspringboot/service/AgencyServiceTest.java
@@ -1,9 +1,8 @@
 package com.example.testspringboot.service;
 
-import com.example.testspringboot.model.Bike;
-import com.example.testspringboot.model.Client;
-import com.example.testspringboot.model.StandardBike;
-import com.example.testspringboot.model.State;
+import com.example.testspringboot.exception.BikeNotAvailable;
+import com.example.testspringboot.exception.LocationNotFound;
+import com.example.testspringboot.model.*;
 import com.example.testspringboot.repository.BikeRepository;
 import com.example.testspringboot.repository.ClientRepository;
 import com.example.testspringboot.repository.LocationRepository;
@@ -14,9 +13,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -37,6 +39,7 @@ public class AgencyServiceTest {
     Client client01;
     Bike bike01;
     Bike bike02;
+    Location location01;
 
 
     @BeforeEach
@@ -52,7 +55,12 @@ public class AgencyServiceTest {
                 .build();
 
         bike02 = StandardBike.builder()
-                .state(State.AVAILABLE)
+                .state(State.RENTED)
+                .build();
+
+        location01 = Location.builder()
+                .client(client01)
+                .bike(bike01)
                 .build();
     }
 
@@ -64,7 +72,52 @@ public class AgencyServiceTest {
 
         List<Bike> result = agencyService.getBikes();
         assertEquals(2, result.size());
+    }
 
+    @Test
+    public void checkAvailabilityTrue() {
+        assertTrue(agencyService.checkAvailability(bike01));
+    }
+
+    @Test
+    public void checkAvailabilityFalse() {
+        assertThrows(BikeNotAvailable.class, () -> agencyService.checkAvailability(bike02));
+    }
+
+    @Test
+    public void testAddNewLocation() {
+        when(bikeRepository.findById(bike01.getId()))
+                .thenReturn(Optional.of(bike01));
+
+        when(clientRepository.findById(client01.getId()))
+                .thenReturn(Optional.of(client01));
+
+        when(bikeRepository.save(bike01))
+                .thenReturn(bike01);
+
+        when(locationRepository.save(any(Location.class)))
+                .thenReturn(new Location());
+
+        agencyService.addNewLocation(client01.getId(), bike01.getId());
+        assertEquals(State.RENTED, bike01.getState());
+
+    }
+
+    @Test
+    public void testEndLocation() {
+
+        when(locationRepository.findByBikeIdAndEndDateIsNull(bike01.getId()))
+                .thenReturn(Optional.ofNullable(location01));
+
+        when(bikeRepository.save(bike01))
+                .thenReturn(bike01);
+
+        when(locationRepository.save(any(Location.class)))
+                .thenReturn(new Location());
+
+        agencyService.endLocation(bike01.getId());
+        assertEquals(State.AVAILABLE, bike01.getState());
+        assertNotNull(location01.getEndDate());
 
     }
 
